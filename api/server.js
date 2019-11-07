20a058cf2128b80498fd27567ab42f8dda7cf796
Carl-Stanley <cds.beml@gmail.com>
2019-11-07 09:50:36 -0700

    Commit

--- api/server.js
+++ api/server.js
@@ -1,33 +1,33 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require("express");
+const helmet = require("helmet");
 
-const Shoutouts = require('../data/shoutouts-model.js');
+const db = require("../data/db.js");
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 
-server.get('/', async (req, res) => {
+server.get("/", async (req, res) => {
   try {
-    const shoutouts = await db('shoutouts');
-    const messageOfTheDay = process.env.MOTD || 'Hello World!'; // add this line
-    res.status(200).json({ motd: messageOfTheDay, shoutouts }); // change this line
+    const shouts = await db("shouts");
+    res.status(200).json({ messageOfTheDay: process.env.MOTD, shouts });
   } catch (error) {
-    console.error('\nERROR', error);
-    res.status(500).json({ error: 'Cannot retrieve the shoutouts' });
+    console.error("\nERROR", error);
+    res.status(500).json({ error: "Cannot retrieve the shouts" });
   }
 });
 
-server.post('/', (req, res) => {
-  Shoutouts.add(req.body)
-  .then(shoutout => {
-    res.status(201).json(shoutout);
-  })
-  .catch (error => {
-    console.error('\nERROR', error);
-    res.status(500).json({ error: 'Cannot add the shoutout' });
-  });
+server.post("/", async (req, res) => {
+  try {
+    const [id] = await db("shouts").insert(req.body);
+    const shouts = await db("shouts");
+
+    res.status(201).json(shouts);
+  } catch (error) {
+    console.error("\nERROR", error);
+    res.status(500).json({ error: "Cannot add the shout" });
+  }
 });
 
 module.exports = server;
